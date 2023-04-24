8a9c78a712a4bb13de7a184a5c0e70be4e23839f
Irfan Nugroho <irfangrh378@gmail.com>
2023-04-24 08:59:17 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS');
+
+    const { url, method } = request;
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Homepage'
+            }));
+        } else {
+            response.statusCode = 500;
+            response.end(JSON.stringify({
+                message: `fail to access ${method}, request`
+            }));
+        }
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Aboutpage'
+            }));
+        } else if (method === 'POST') {
+            let body = [];
+
+            request.on('data', chunk => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(JSON.stringify({
+                    message: `Hello, ${name} this is about page`
+                }));
+            });
+        } else {
+            response.statusCode = 500;
+            response.end(JSON.stringify({
+                message: `fail to access ${method}, request`
+            }));
+        }
+    } else {
+        response.statusCode = 404;
+        response.end(JSON.stringify({
+            message: 'not found'
+        }));
+    }
+};
+
+const server = http.createServer(requestListener);
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
